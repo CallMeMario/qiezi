ad31fda9e5379dfe8f5acd16a5d318c1e6498b73
lihao <2864663218@qq.com>
2020-09-11 12:03:33 +0800

    秋梨膏

--- src/com/hello.java
+++ src/com/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("A1高闪来一个好吗？");
         System.out.println("A1高闪来一个好吗？");
+        System.out.println("A1高闪来一个好吗？");
     }
 }
