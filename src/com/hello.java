c29f976f64fd84d99638c6f97879fe5ee4b657f9
CallMeMario <71064375+CallMeMario@users.noreply.github.com>
2020-09-11 12:06:13 +0800

    Update hello.java

--- src/com/hello.java
+++ src/com/hello.java
@@ -5,5 +5,6 @@ public class hello {
         System.out.println("A1高闪来一个好吗？");
         System.out.println("A1高闪来一个好吗？");
         System.out.println("A1高闪来一个好吗？");
+        System.out.println("A1高闪来一个好吗？");
     }
 }
