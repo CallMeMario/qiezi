0cf2a9f139b7278da43d19ef4cb0e56b5aa6daf9
CallMeMario <2864663218@qq.com>
2020-12-02 22:34:29 +0800

    新建分支修改

--- src/com/hello.java
+++ src/com/hello.java
@@ -11,6 +11,7 @@ public class hello {
         System.out.println("A1高闪来一个好吗？，不好");
         System.out.println("耗子尾汁");
         System.out.println("不讲武德");
-        System.out.println("左刺拳，右鞭腿");
+        System.out.println("左刺拳， 右鞭腿 ");
+        System.out.println("左刺拳， 右鞭腿 ");
     }
 }
